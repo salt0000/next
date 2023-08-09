71cb69dc7d61c626ac85dbab5712bb5bb99b5ed6
sal0000 <shio.kunisawa@gmail.com>
2023-08-09 10:09:11 +0900

    update

--- client/src/app/hoge/page.tsx
+++ client/src/app/hoge/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Hoge() {
   return (
     <main>
+      <div>テスト</div>
       {process.env.NEXT_PUBLIC_URL}
       <br/>
       {process.env.NEXT_PUBLIC_HOGE}
