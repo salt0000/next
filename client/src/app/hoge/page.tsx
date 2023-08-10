4db39f3606ffc2071ad17146aa2c07d445598e1d
sal0000 <shio.kunisawa@gmail.com>
2023-08-10 12:11:17 +0900

    UPDATE

--- client/src/app/hoge/page.tsx
+++ client/src/app/hoge/page.tsx
@@ -8,6 +8,7 @@ export default function Hoge() {
       <div>テスト3</div>
       <div>テスト4</div>
       <div>テスト5</div>
+      <div>テスト6</div>
       {process.env.NEXT_PUBLIC_URL}
       <br/>
       {process.env.NEXT_PUBLIC_HOGE}
